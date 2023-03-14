0c3452fce68e66e9fd3b942ee47c6b0f8fe06b10
dannyhankk <langzihankang@hotmail.com>
2023-03-14 16:21:50 +0800

    fix gen voice

--- client/ms_voice_client.go
+++ client/ms_voice_client.go
@@ -2,13 +2,16 @@ package client
 
 import (
 	"fmt"
+	pb "github.com/dannyhankk/cognitive/proto"
 	"strings"
 )
 
 const (
-	ssmlHeaderStart = "<speak version=\"1.0\" xmlns=\"http://www.w3.org/2001/10/synthesis\" xml:lang=\""
-	ssmlHeaderEnd   = "\">"
-	ssmlEnd         = "</speak>"
+	ssmlHeaderStart = "speak xmlns=\"http://www.w3.org/2001/10/synthesis\" " +
+		"xmlns:mstts=\"http://www.w3.org/2001/mstts\" xmlns:emo=\"http://www.w3.org/2009/10/emotionml\" " +
+		"version=\"1.0\" xml:lang=\""
+	ssmlHeaderEnd = "\">"
+	ssmlEnd       = "</speak>"
 
 	ssmlVoiceHeaderStart = "<voice name=\""
 	ssmlVoiceHeaderEnd   = "\">"
@@ -19,7 +22,7 @@ type MSText2SpeechOpt struct {
 	output string
 }
 
-func transText2SSML(text string, lang int32) (string, error) {
+func transText2SSML(text string, lang pb.LangType) (string, error) {
 	ssml := ssmlHeaderStart + getLang(lang) + ssmlHeaderEnd
 	first := strings.Split(text, "Male:")
 	for _, firstItem := range first {
@@ -47,16 +50,70 @@ func transText2SSML(text string, lang int32) (string, error) {
 }
 
 // return value: female male
-func getVoiceName(lang int32, isMale bool) string {
+func getVoiceName(lang pb.LangType, isMale bool) string {
 	switch lang {
-	case 1:
-		return ""
-
+	case pb.LangType_ENGLISH:
+		if isMale {
+			return "en-US-JasonNeural"
+		}
+		return "en-US-JennyNeural"
+	case pb.LangType_RUSSIA:
+		if isMale {
+			return "ru-RU-DmitryNeural"
+		}
+		return "ru-RU-DariyaNeural"
+	case pb.LangType_FRENCH:
+		if isMale {
+			return "fr-FR-ClaudeNeural"
+		}
+		return "fr-FR-DeniseNeural"
+	case pb.LangType_SPANISH:
+		if isMale {
+			return "es-ES-DarioNeural"
+		}
+		return "es-ES-ElviraNeural"
+	case pb.LangType_PORTUGAL:
+		if isMale {
+			return "pt-PT-DuarteNeural"
+		}
+		return "pt-PT-FernandaNeural"
+	case pb.LangType_JAPANESE:
+		if isMale {
+			return "ja-JP-KeitaNeural"
+		}
+		return "ja-JP-NanamiNeural"
+	case pb.LangType_KOREAN:
+		if isMale {
+			return "ko-KR-InJoonNeural"
+		}
+		return "ko-KR-SoonBokNeural"
+	case pb.LangType_ARABIC:
+		if isMale {
+			return "ar-SA-HamedNeural"
+		}
+		return "ar-SA-ZariyahNeural"
 	}
-
-	return ""
+	return "en-US"
 }
 
-func getLang(lang int32) string {
-	return "en-us"
+func getLang(lang pb.LangType) string {
+	switch lang {
+	case pb.LangType_ENGLISH:
+		return "en-US"
+	case pb.LangType_RUSSIA:
+		return "ru-RU"
+	case pb.LangType_FRENCH:
+		return "fr-FR"
+	case pb.LangType_SPANISH:
+		return "es-ES"
+	case pb.LangType_PORTUGAL:
+		return "pt-PT"
+	case pb.LangType_JAPANESE:
+		return "ja-JP"
+	case pb.LangType_KOREAN:
+		return "ko-KR"
+	case pb.LangType_ARABIC:
+		return "ar-SA"
+	}
+	return "en-US"
 }
